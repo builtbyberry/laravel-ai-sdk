da21d8cdeafd43a3aab45a9ca9efcd7f310312bd
dberry37388 <dberry37388@users.noreply.github.com>
2024-08-26 16:25:02 +0000

    Fix styling

--- src/LaravelAiSdkServiceProvider.php
+++ src/LaravelAiSdkServiceProvider.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace BuiltByBerry\LaravelAiSdk;
 
-use BuiltByBerry\LaravelAiSdk\Commands\LaravelAiSdkCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
