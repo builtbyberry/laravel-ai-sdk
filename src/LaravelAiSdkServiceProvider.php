92233d352462babb767130d65415b393e1b8dd44
Daniel Berry <daniel.berry@rimsys.io>
2024-08-26 11:22:03 -0500

    chore: remove Command directory and clean up ServiceProvider

--- src/LaravelAiSdkServiceProvider.php
+++ src/LaravelAiSdkServiceProvider.php
@@ -6,23 +6,14 @@ namespace BuiltByBerry\LaravelAiSdk;
 
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use BuiltByBerry\LaravelAiSdk\Commands\LaravelAiSdkCommand;
 
 class LaravelAiSdkServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('laravel-ai-sdk')
-            ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_laravel_ai_sdk_table')
-            ->hasCommand(LaravelAiSdkCommand::class);
+            ->hasConfigFile();
     }
 
     public function register(): void
